8b27521b66154a693ca4cc562730b341ae05609f
Aditya Singh <adityasingh222247@gmail.com>
2019-05-19 22:16:00 +0530

    Synchronizing POST Additions

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -41,14 +41,19 @@ class Feed extends Component {
 			.catch(this.catchError);
 
 		this.loadPosts();
-		openSocket('http://localhost:8000')
+		const socket = openSocket('http://localhost:8000')
+		socket.on('posts', data => {
+			console.log('entered')
+			if (data.action === 'create') {
+				this.addPost(data.post)
+			}
+		})
 	}
 
 	addPost = (post) => {
 		this.setState(prevState => {
 			const updatedPosts = [...prevState.posts]
 			if (prevState.postPage === 1) {
-				updatedPosts.pop()
 				updatedPosts.unshift(post)
 			}
 			return {
@@ -181,8 +186,6 @@ class Feed extends Component {
 							p => p._id === prevState.editPost._id
 						);
 						updatedPosts[postIndex] = post;
-					} else if (prevState.posts.length < 2) {
-						updatedPosts = prevState.posts.concat(post);
 					}
 					return {
 						posts: updatedPosts,
